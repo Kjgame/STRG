38337224bf44eb8ac6276e0405a1b6705ada8ee3
DunklerHeld <dunklerheld2@gmail.com>
2017-11-17 14:59:30 +0100

    Commit #1

---
+++ src/game/Start.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package game;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author erik.erbsloeh
+ */
+public class Start {
+    
+    
+    
+    public static void start(String username) {
+        JOptionPane.showMessageDialog(null, username);
+    }
+    
+}
