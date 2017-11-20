370ef791094cb3e6c10e983e1c739864b8488f48
DunklerHeld <dunklerheld2@gmail.com>
2017-11-20 20:40:43 +0100

    Create database_test.sql
    An sql file to quickly create a test table.

---
+++ sql/database_test.sql
@@ -0,0 +1,4 @@
+create table test.baum(nr int not null PRIMARY KEY, name varchar(100) not null);
+insert into test.baum(nr, name) values (0, "eiche");
+insert into test.baum(nr, name) values (1, "esche");
+insert into test.baum(nr, name) values (2, "birke");
